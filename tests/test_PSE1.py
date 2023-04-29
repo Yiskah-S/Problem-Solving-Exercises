add19286e7473a614ddf3da90d309dcc1e01a040
Jessica <Yiskah.Scientia@gmail.com>
2023-04-29 13:25:48 -0600

    PSE 1

--- tests/test_PSE1.py
+++ tests/test_PSE1.py
@@ -1,29 +1,29 @@
-# import pytest
-# from PSE1_highest_rated.PSE1 import get_highest_rated
+import pytest
+from PSEs.PSE1_highest_rated import get_highest_rated
 
 
-# def test_get_highest_rating_in_multiple_restaurants_list():
-#     # ^rename with meaningful test name
-#     # and complete test implementation below
-#     # arrange
-#     restaurants = [{"name": "Grillby's", "rating": 1}, {"name": "Crow's Nest", "rating": 5}]
+def test_get_highest_rating_in_multiple_restaurants_list():
+    # ^rename with meaningful test name
+    # and complete test implementation below
+    # arrange
+    restaurants = [{"name": "Grillby's", "rating": 1}, {"name": "Crow's Nest", "rating": 5}]
 
-#     # act
-#     result = get_highest_rated(restaurants)
+    # act
+    result = get_highest_rated(restaurants)
 
-#     # assert
-#     assert result == {"name": "Crow's Nest", "rating": 5}
+    # assert
+    assert result == {"name": "Crow's Nest", "rating": 5}
 
-# def test_empty_restaurants_list():
-#     # ^rename with meaningful test name
-#     # and complete test implementation below
-#     pass
+def test_empty_restaurants_list():
+    # ^rename with meaningful test name
+    # and complete test implementation below
+    pass
     
-#     # arrange
-#     restaurants = []
+    # arrange
+    restaurants = []
     
-#     # act
-#     result = get_highest_rated(restaurants)
+    # act
+    result = get_highest_rated(restaurants)
     
-#     # assert
-#     assert result == None
+    # assert
+    assert result == None
