603622052ecd5c02b82e2d8ea2d3d8db6b964b6d
Jessica <Yiskah.Scientia@gmail.com>
2023-05-06 14:46:45 -0600

    PSE6 tests

---
+++ PSEs/PSE6_merging_sorted_lists.py
@@ -0,0 +1,37 @@
+list1 = [1, 2, 3, 4]
+list2 = [1, 2, 3]
+
+def merge_lists(list1, list2):
+    if not (list1 or list2):
+        raise ValueError("Both lists are empty")
+    
+    if not all(isinstance(lst, list) for lst in (list1, list2)):
+        raise TypeError("Invalid input, lists must be a list")
+
+    for num in list1 + list2:
+        if not isinstance(num, (int, float)):
+            raise TypeError("Invalid input in list")
+        
+    # list1 = list(filter(lambda num: num not in list2, list1))
+    if len(set(list1 + list2)) != len(list1 + list2):
+        duplicates = set()
+        for num1 in list1:
+            for num2 in list2:
+                if num1 == num2:
+                    duplicates.add(num1)
+                    break
+        for num in list1:
+            if num in duplicates:
+                list1.remove(num)
+
+    if not list1:
+        return list2
+    elif not list2:
+        return list1
+    elif list1[0] < list2[0]:
+        return [list1[0]] + merge_lists(list1[1:], list2)
+    else:
+        return [list2[0]] + merge_lists(list1, list2[1:])
+
+test = merge_lists(list1, list2)
+print(f"{test = }")
--- tests/test_PSE6.py
+++ tests/test_PSE6.py
@@ -1,2 +1,83 @@
-# import pytest
-# from PSE6.PSE6 import
+import pytest
+from PSEs.PSE6_merging_sorted_lists import merge_lists
+
+def test_both_lists_are_empty():
+    list1 = []
+    list2 = []
+
+    try:
+        result = merge_lists(list1, list2)
+    except ValueError as err:
+        assert str(err) == "Both lists are empty"
+    else:
+        assert False, "Expected ValueError wasn't raised"
+
+def test_list_contains_non_numerals():
+    list1 = [2, 1, 3]
+    list2 = ["invalid"]
+
+    try:
+        result = merge_lists(list1, list2)
+    except TypeError as err:
+        assert str(err) == "Invalid input in list"
+    else:
+        assert False, "Expected TypeError wasn't raised"
+
+def test_returns_sorted_list_if_one_is_empty_at_start():
+    list1 = [1, 2, 3]
+    list2 = []
+
+    result = merge_lists(list1, list2)
+
+    assert result == [1, 2, 3]
+
+def test_input_not_list_raises_error():
+    list1 = "invalid"
+    list2 = 3
+
+    try:
+        result = merge_lists(list1, list2)
+    except TypeError as err:
+        assert str(err) == "Invalid input, lists must be a list"
+    else:
+        assert False, "Expected TypeError wasn't raised"
+
+def test_lists_same_lenght_return_correctly():
+    list1 = [1, 2, 3]
+    list2 = [4, 5, 6]
+
+    result = merge_lists(list1, list2)
+
+    assert result == [1, 2, 3, 4, 5, 6]
+
+def test_lists_different_lenght_return_correctly():
+    list1 = [1, 2, 3]
+    list2 = [4]
+
+    result = merge_lists(list1, list2)
+
+    assert result == [1, 2, 3, 4]
+
+def test_negative_nums_return_correctly():
+    list1 = [-2, 1, 3]
+    list2 = [-5, 4, 6]
+
+    result = merge_lists(list1, list2)
+
+    assert result == [-5, -2, 1, 3, 4, 6]
+
+def test_floats_return_correctly():
+    list1 = [1, 2.2, 3]
+    list2 = [4, 5.5, 6]
+
+    result = merge_lists(list1, list2)
+
+    assert result == [1, 2.2, 3, 4, 5.5, 6]
+
+def test_lists_with_duplicates_correctly():
+    list1 = [1, 2, 3]
+    list2 = [1, 2, 3]
+
+    result = merge_lists(list1, list2)
+
+    assert result == [1, 2, 3]
